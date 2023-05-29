18d372abd3b739296f80294424149d63313c165b
asdadsasdxdc <official.grawell@gmail.com>
2023-05-30 01:46:30 +0300

    commit

--- lib/data/categories.dart
+++ lib/data/categories.dart
@@ -6,23 +6,25 @@ import '../models/bubo_category.dart';
 
 /// Original labels in german and bulgarian, english added as last value
 const labels = [
-  ["1", "Obst", "плодове", "vegetables", "ES"],
-  ["2", "Farben", "цветове", "colors"],
-  ["3", "Moebel", "мебели", "furniture"],
-  ["4", "Sachen", "дрехи", "clothes"],
-  ["5", "Spielzeug", "играчки", "toys"],
-  ["6", "Tiere", "животни", "animals"],
-  ["7", "Wilde Tiere", "диви животни", "wild animals"],
-  ["8", "Zahlen", "цифри", "digits"],
-  ["9", "Wetter", "времето", "weather"],
-  ["10", "Zu Hause", "вкъщи", "at home"],
-  ["11", "Natur", "природа", "nature"],
-  ["12", "Im Meer", "морето", "sea"],
-  ["13", "Am Strand", "на плажа", "on the beach"],
-  ["14", "Sport", "спорт", "sport"],
-  ["15", "Stadt", "град", "city"],
-  ["16", "Berufe", "професии", "professions"],
-  ["17", "Fahrzeuge", "транспрорт", "transport"]
+  ["1", "Nissan GTR", "Nissan GTR", "Nissan GTR"],
+  ["2", "Subaru Impreza", "Subaru Impreza", "Subaru Impreza"],
+  ["3", "Toyota Supra", "Toyota Supra", "Toyota Supra"],
+  ["4", "Dodge Challenger SRT", "Dodge Challenger SRT", "Dodge Challenger SRT"],
+  ["5", "BMW", "BMW", "BMW"],
+  ["6", "Fiat Punto Sporting", "Fiat Punto Sporting", "Fiat Punto Sporting"],
+  ["7", "Volkswagen Golf GTI", "Volkswagen Golf GTI", "Volkswagen Golf GTI"],
+  ["8", "Mercedes Benz AMG", "Mercedes Benz AMG", "Mercedes Benz AMG"],
+  ["9", "Lamborghini Urus", "Lamborghini Urus", "Lamborghini Urus"],
+  ["10", "Ferrari", "Ferrari", "Ferrari"],
+  ["11", "Mazda Miata", "Mazda Miata", "Mazda Miata"],
+  ["12", "Peugeot RS", "Peugeot RS", "Peugeot RS"],
+  ["13", "Bugatti Chiron", "Bugatti Chiron", "Bugatti Chiron"],
+  ["14", "Reno Megan Sport", "Reno Megan Sport", "Reno Megan Sport"],
+  ["15", "Sin R", "Sin R", "Sin R"],
+  ["16", "Porsche", "Porsche", "Porsche"],
+  ["17", "Citroen", "Citroen", "Citroen"],
+  ["18", "Lexus LFA", "Lexus LFA", "Lexus LFA"],
+  ["19", "Honda NSX", "Honda NSX", "Honda NSX"],
 ];
 
 ///
@@ -35,8 +37,8 @@ List<BuboCategory> buboCategories = labels.map((categoryLabels) {
       int.parse(categoryLabels[0]),
       'assets/categories/cat_${categoryLabels[1].toLowerCase().replaceAll(' ', '_')}@3x.png',
       {
-        'us': categoryLabels[3],
-        'de': categoryLabels[1],
-        'bg': categoryLabels[2]
+        'us': categoryLabels[1],
+        'de': categoryLabels[3],
+        'bg': categoryLabels[2],
       });
 }).toList();
