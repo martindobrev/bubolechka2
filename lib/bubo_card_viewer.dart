1d8f7d592fda8b03a8121c386851995880897976
Martin Dobrev <martin.dobrev@akros.ch>
2023-06-12 19:19:09 +0300

    normal audio player added

--- lib/bubo_card_viewer.dart
+++ lib/bubo_card_viewer.dart
@@ -1,5 +1,6 @@
 import 'package:bubolechka2/models/bubo_card.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 ///
 /// Widget displaying scrollable pages for each card in a selected category
@@ -8,8 +9,9 @@ import 'package:flutter/material.dart';
 ///
 class BuboCardViewer extends StatelessWidget {
   final List<BuboCard> cards;
+  final AudioPlayer audioPlayer = AudioPlayer();
 
-  const BuboCardViewer(this.cards, {super.key});
+  BuboCardViewer(this.cards, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -46,9 +48,15 @@ class BuboCardViewer extends StatelessWidget {
       (card) {
         return Padding(
           padding: const EdgeInsets.all(100.0),
-          child: Center(
-              child: Image.asset(
-                  'assets/cards/${card.file.replaceAll('.aac', '@3x.png')}')),
+          child: GestureDetector(
+            onTap: () {
+              audioPlayer
+                  .play(DeviceFileSource('assets/voice/${card.audioFile}'));
+            },
+            child: Center(
+                child: Image.asset(
+                    'assets/cards/${card.file.replaceAll('.aac', '@3x.png')}')),
+          ),
         );
       },
     ).toList();
